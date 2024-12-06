a4abf8a7323e3094ad521491e68e0b9fc68a2d02
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-06 19:57:21 +0100

    day 5 | two stars

--- day5/day5.py
+++ day5/day5.py
@@ -25,17 +25,25 @@ def one():
   print(result)
 
 def two():
+  from functools import cmp_to_key
+  def compare(x, y):
+    if (x,y) in dd: return -1
+    if (y,x) in dd: return 1
+    return 0
+
   d = {}
+  dd = set()
   result = 0
   with open('input.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
       if '|' in line:
-        l, r = line.replace('\n','').split('|')
+        l, r = [x for x in line.replace('\n','').split('|')]
         if l in d:
           d[l].append(r)
         else:
           d[l] = [r]
+        dd.add((l, r))
       if ',' in line:
         numbers = line.replace('\n', '').split(',')
         done = set()
@@ -47,7 +55,7 @@ def two():
                 good = False
           done.add(x)
         if not good:
-          ...
+          numbers.sort(key=cmp_to_key(compare))
           result += int(numbers[len(numbers)//2])
   print(result)
   pass
