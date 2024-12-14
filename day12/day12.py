199ae7212c459d0034f3bc8a2d13202d70ac5876
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-14 11:37:08 +0100

    day 12 | two stars

--- day12/day12.py
+++ day12/day12.py
@@ -1,6 +1,3 @@
-#import sys
-#sys.setrecursionlimit(100_000)
-
 def calc(m,i,j,v,d):
   inside = lambda x,y: x>=0 and x<len(m) and y>=0 and y<len(m[0])
   if (i,j) in v: return
@@ -19,8 +16,26 @@ def calc(m,i,j,v,d):
   if inside(i, j-1) and m[i][j-1] == m[i][j]: calc(m,i,j-1,v,d) 
   if inside(i-1, j) and m[i-1][j] == m[i][j]: calc(m,i-1,j,v,d) 
     
-def calc2(m,i,j,v,vv,d):
-  pass
+  
+def calc2(m,i,j,v,d):
+  inside = lambda x,y: x>=0 and x<len(m) and y>=0 and y<len(m[0])
+  v.add((i,j))
+  d[0]+=1
+  dir = [(0,1),(-1,0),(0,-1),(1,0)]
+  for idx in range(len(dir)):
+    a,b = dir[idx]
+    a2,b2 = dir[(idx+1)%4]
+    if not good(m,i,j,i+a,j+b) and not good(m,i,j,i+a2,j+b2):
+      d[1] += 1
+    if good(m,i,j,i+a,j+b) and good(m,i,j,i+a2,j+b2) and not good(m,i,j,i+a+a2,j+b+b2):
+      d[1] += 1
+  for a,b in dir:
+    if good(m,i,j,i+a,j+b) and (i+a,j+b) not in v:
+      calc2(m,i+a,j+b,v,d)
+
+def good(m,i,j,i2,j2):
+  inside = lambda x,y: x>=0 and x<len(m) and y>=0 and y<len(m[0])
+  return inside(i2,j2) and (m[i][j] == m[i2][j2])
 
 def one():
   m = []
@@ -48,10 +63,9 @@ def two():
   for i in range(len(m)):
     for j in range(len(m[i])):
       d = [0,0]
-      vv = dict()
       if (i,j) not in v:
-        calc2(m,i,j,v,vv,d)
-      if d[0] != 0: print(m[i][j],d[0],d[1],  vv[m[i][j]])
+        calc2(m,i,j,v,d)
+      #if d[0] != 0: print(m[i][j],d[0],d[1])
       result += d[0]*d[1]
   print(f'result2: {result}')
 
