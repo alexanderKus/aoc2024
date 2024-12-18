bae8f2bab577e928c8a67074a8443a63c464bc84
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-18 23:33:20 +0100

    day 16 | one star

--- day16/day16.py
+++ day16/day16.py
@@ -1,5 +1,26 @@
+import heapq
+
 def one():
-  print('Hello, World!')
+  maze = []
+  with open('input.txt', 'r') as f:
+    for line in f.readlines():
+      maze.append(list(line.replace('\n','')))
+  sx,sy,ex,ey = -1,-1,-1,-1
+  for i in range(len(maze)):
+    for j in range(len(maze[i])):
+      if maze[i][j] == 'S': sx,sy = i,j
+      if maze[i][j] == 'E': ex,ey = i,j
+  pq = [(0,sx,sy,0,1)]
+  visited = {(sx,sy,0,1)}
+  while pq:
+    cost,row,col,dx,dy = heapq.heappop(pq)
+    visited.add((row,col,dx,dy))
+    if (row,col) == (ex,ey):
+      print(cost)
+      break
+    for new_cost, nx, ny, ndx, ndy in [(cost+1, row+dx, col+dy, dx,dy),(cost+1000, row, col, dy,-dx), (cost+1000, row, col, -dy,dx)]:
+      if maze[nx][ny] == '#' or (nx,ny,ndx,ndy) in visited: continue
+      heapq.heappush(pq,(new_cost,nx,ny,ndx,ndy))
 
 def two():
   pass
