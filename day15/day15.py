9e6f4dfb149d33b91a83cd34bc03ed94cddf7916
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-18 19:25:31 +0100

    Day 15 | two stars

--- day15/day15.py
+++ day15/day15.py
@@ -59,6 +59,71 @@ def two():
   warehouse = []
   moves = []
   dirs = {'>':(0,1), 'v':(1,0), '<':(0,-1), '^':(-1,0)}
+  with open('input.txt', 'r') as f:
+    m = False
+    for line in f.readlines():
+      if line == '\n': 
+        m = True
+        continue
+      if m: 
+        for move in line.replace('\n',''):
+          moves.append(move)
+      else: warehouse.append([x for x in line.replace('\n','')])
+  new_warehouse = []
+  for i in range(len(warehouse)):
+    new_warehouse.append([])
+    for j in range(len(warehouse[i])):
+      if warehouse[i][j] == '@': 
+        new_warehouse[i].append('@')
+        new_warehouse[i].append('.')
+      elif warehouse[i][j] == '.': 
+        new_warehouse[i].append('.')
+        new_warehouse[i].append('.')
+      elif warehouse[i][j] == '#': 
+        new_warehouse[i].append('#')
+        new_warehouse[i].append('#')
+      elif warehouse[i][j] == 'O': 
+        new_warehouse[i].append('[')
+        new_warehouse[i].append(']')
+  warehouse = new_warehouse
+  rx, ry = -1,-1
+  for i in range(len(warehouse)):
+    for j in range(len(warehouse[i])):
+      if warehouse[i][j] == '@':
+        rx,ry = i,j
+  for index, move in enumerate(moves):
+    print(f'index: {index}, move: {move}')
+    dx, dy = dirs[move]
+    targets = [(rx,ry)]
+    go = True
+    for cx,cy in targets:
+      nx,ny = cx+dx,cy+dy
+      if (nx,ny) in targets: continue
+      if warehouse[nx][ny] == '#':
+        go = False
+        break
+      if warehouse[nx][ny] == '[':
+        targets.append((nx,ny))
+        targets.append((nx,ny+1))
+      if warehouse[nx][ny] == ']':
+        targets.append((nx,ny))
+        targets.append((nx,ny-1))
+    if not go: continue
+    copy = [list(x) for x in warehouse]
+    warehouse[rx][ry] = '.'
+    warehouse[rx+dx][ry+dy] = '@'
+    for bx,by in targets[1:]: warehouse[bx][by] = '.'
+    for bx,by in targets[1:]: warehouse[bx+dx][by+dy] = copy[bx][by]
+    rx += dx
+    ry += dy
+
+  result = 0
+  for i in range(len(warehouse)):
+    for j in range(len(warehouse[i])):
+      if warehouse[i][j] == '[':
+        result += 100*i+j
+  print(f'result2: {result}')
+
 if __name__ == '__main__':
-  one()
+  #one()
   two()
---
+++ day16/day16.py
@@ -0,0 +1,9 @@
+def one():
+  print('Hello, World!')
+
+def two():
+  pass
+
+if __name__ == '__main__':
+  one()
+  two()
