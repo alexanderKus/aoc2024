a68f8f4d9a5ee87ae661956b3413c092beda5f7c
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-21 21:08:30 +0100

    day 19 | two stars

--- day19/day19.py
+++ day19/day19.py
@@ -1,13 +1,11 @@
+from functools import cache
 
 def valid(towel, patterns):
-  if towel == '': 
-    #print('end')
-    return True
+  if towel == '': return True
   for pattern in patterns:
     v = False
     if towel[:len(pattern)] == pattern:
       if valid(towel[len(pattern):],patterns): 
-        #print(towel[len(pattern):])
         v = True
     if v: return True
   return False
@@ -17,15 +15,28 @@ def one():
     data = f.readlines()
   patterns = sorted([x.strip() for x in data[0].replace('\n','').split(',')],key=len,reverse=True)
   towels = [x.replace('\n','') for x in data[2:]]
-  count = 0
-  for towel in towels:
-    if valid(towel, patterns): 
-      #print(f'valid towel: {towel}')
-      count += 1
+  count = sum(1 if valid(towel,patterns) else 0 for towel in towels)
   print(f'Result1: {count}')
 
+
 def two():
-  ...
+  with open('input.txt','r') as f:
+    data = f.readlines()
+  patterns = sorted([x.strip() for x in data[0].replace('\n','').split(',')],key=len,reverse=True)
+  towels = [x.replace('\n','') for x in data[2:]]
+
+  @cache
+  def valid2(towel):
+    if towel == '': return 1
+    c = 0
+    nonlocal patterns
+    for pattern in patterns:
+      if towel[:len(pattern)] == pattern:
+        c += valid2(towel[len(pattern):])
+    return c
+
+  count = sum(valid2(towel) for towel in towels)
+  print(f'Result2: {count}')
 
 if __name__ == '__main__':
   one()
