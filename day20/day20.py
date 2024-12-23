4f82354a58b4ec540a16ab920577ce2b4127ec9a
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-23 20:43:41 +0100

    day 20 | two stars

--- day20/day20.py
+++ day20/day20.py
@@ -38,7 +38,6 @@ def one():
       if grid[x][y] == 'E':
         nonlocal costs
         nonlocal init_cost
-        print(init_cost - cost)
         costs.append(init_cost - cost)
         break
       for (i,j) in dirs:
@@ -58,7 +57,6 @@ def one():
         bfs()
         grid[i][j] = '#'
   
-  #sorted_costs = dict(sorted(costs.items(), key=lambda item: item[1]))
   sorted_costs = dict(sorted((Counter(costs).most_common()), reverse=True))
   print(sorted_costs)
   result = 0
@@ -69,7 +67,38 @@ def one():
 
 
 def two():
-  ...
+  with open('input.txt','r') as f:
+    grid = [list(line.strip()) for line in f.readlines()]
+
+  sx,sy = -1,-1
+  for i in range(len(grid)):
+    for j in range(len(grid[i])):
+      if grid[i][j] == 'S': sx,sy = i,j
+  
+  dists = [[-1]*len(grid[0]) for _ in range(len(grid))]
+  dists[sx][sy] = 0
+
+  while grid[sx][sy] != 'E':
+    for i,j in dirs:
+      dx,dy = sx+i,sy+j
+      if not inside(grid,dx,dy): continue
+      if grid[dx][dy] == '#': continue
+      if dists[dx][dy] != -1: continue
+      dists[dx][dy] = dists[sx][sy] + 1
+      sx,sy = dx,dy
+
+  count = 0
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):
+      if grid[r][c] == '#': continue
+      for radius in range(2,21):
+        for dr in range(radius+1):
+          dc = radius - dr
+          for nr, nc in {(r+dr,c+dc),(r+dr,c-dc),(r-dr,c+dc),(r-dr,c-dc)}:
+            if not inside(grid,nr,nc): continue
+            if grid[nr][nc] == '#': continue
+            if dists[r][c] - dists[nr][nc] >= 100 + radius: count += 1
+  print(f'result2: {count}')
 
 if __name__ == '__main__':
   one()
