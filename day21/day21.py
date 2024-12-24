c356e5cd935c8e74b6899dda24b57594b3591bce
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-24 14:49:17 +0100

    day 21 | init

---
+++ day21/day21.py
@@ -0,0 +1,27 @@
+from collections import deque
+import pprint
+
+grid = [[7,8,9],
+        [4,5,6],
+        [1,2,3],
+       [-1,0,'A']]
+
+pad = [[-1, '^','A'],
+       ['<','v','>']]
+
+GH=4
+GW=3
+PH=2
+PW=3
+grid_paths = dict()
+pad_paths = dict()
+dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+
+def one():
+
+def two():
+  ...
+
+if __name__ == '__main__':
+  one()
+  two()
